519b9e821372804aa22b3da0e60119534907955f
myj1217 <myj1217@naver.com>
2022-03-21 17:55:39 +0900

    Update 1284.py

--- 1284.py
+++ 1284.py
@@ -1,4 +1,13 @@
-n, m = map(int, input().split())
-
-print(n // m)
-print(n % m)
+while 1:
+    N = input()
+    if N == '0':
+        break
+    result = len(N)+1
+    for n in N:
+        if n == '0':
+            result += 4 
+        elif n == '1':
+            result += 2
+        else:
+            result += 3
+    print(result)
