ce0a57393dbfe814682844be9fe97adbd157119f
myj1217 <myj1217@naver.com>
2022-05-23 19:23:02 +0900

    Create 1592.py

---
+++ 1592.py
@@ -0,0 +1,10 @@
+# 1592번 영식이와 친구들
+
+N, M, L = map(int, input().split())
+li = [0]*N
+cnt = i = 0
+while li[i] < M-1:
+    li[i] += 1
+    cnt += 1
+    i = (i+L)%N if li[i]%2 == 1 else (i-L)%N
+print(cnt)
