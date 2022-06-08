9d855c89a881377d9d63b2c69297814f8c948e8d
myj1217 <myj1217@naver.com>
2022-06-08 22:01:51 +0900

    Create 1673.py

---
+++ 1673.py
@@ -0,0 +1,13 @@
+# 1672번. 치킨 쿠폰
+
+while 1:
+    try:
+        n, k = map(int, input().split())
+        res = 0 
+        res += n
+        while n//k:
+            res += n//k
+            n = n//k + n%k
+        print(res)
+    except:
+        break
