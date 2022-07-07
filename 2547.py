3a0eded5e3914145ab2f3332b2324c9b8b51fa60
myj1217 <myj1217@naver.com>
2022-07-07 20:02:52 +0900

    Create 2547.py

---
+++ 2547.py
@@ -0,0 +1,8 @@
+# 2547번: 사탕 선생 고창영
+
+T = int(input())
+for _ in range(T):
+    q = input()
+    N = int(input())
+    li = [int(input()) for i in range(N)]
+    print("YES" if sum(li) % N == 0 else "NO")
