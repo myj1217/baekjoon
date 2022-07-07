649ec1c268595afc279d751ec7b7281c415ddc8e
myj1217 <myj1217@naver.com>
2022-07-07 20:04:05 +0900

    Update 2010.py

--- 2010.py
+++ 2010.py
@@ -1,9 +1,9 @@
-# 1871번: 좋은 자동차 번호판
+# 2010번: 플러그
 
-for _ in range(int(input())):
-    L, D = input().split('-')
-    n = int(D)
-    s = 0
-    for i in range(3):
-        s += (ord(L[i]) - 65) * 26 ** (2 - i)
-    print("nice" if abs(s-n) <= 100 else "not nice")
+import sys 
+
+n = int(input()) 
+s = 0 
+for _ in range(n): 
+    s += int(sys.stdin.readline()) 
+print(s - n + 1)
