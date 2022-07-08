3b9e0bb0476bb945b58c83b9223ee07e80dbafe8
myj1217 <myj1217@naver.com>
2022-07-08 20:45:41 +0900

    Update 2566.py

--- 2566.py
+++ 2566.py
@@ -1,14 +1,23 @@
 # 2566번: 최댓값
 
-max_n = 0
-max_i = 0
-max_j = 0
-for i in range(1, 10):
-    li = list(map(int, input().split()))
-    n = max(li)
-    if max_n < n:
-        max_n = n
-        max_i = i
-        max_j = li.index(max(li)) + 1
-print(max_n)
-print(max_i, max_j)
+import sys
+input = sys.stdin.readline
+
+board = []
+
+for _ in range(9) :
+    board.append(list(map(int, input().split())))
+
+X = 0
+Y = 0
+MAX = -1
+
+for i in range(9) :
+    for j in range(9) :
+        if board[i][j] > MAX :
+            MAX = board[i][j]
+            X = i+1
+            Y = j+1
+
+print(MAX)
+print(X, Y)
