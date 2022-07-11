7831144f8a115487c198e7702b85964c9165458a
myj1217 <myj1217@naver.com>
2022-07-11 21:08:06 +0900

    Create 2576.py

---
+++ 2576.py
@@ -0,0 +1,12 @@
+# 2567번: 홀수
+
+odd_li = []
+for _ in range(7):
+    n = int(input())
+    if n%2:
+        odd_li.append(n)
+if odd_li:
+    print(sum(odd_li))
+    print(min(odd_li))
+else:
+    print(-1)
