aec1a0b6bbe13d8099e0f19871eef4ef8e766979
myj1217 <myj1217@naver.com>
2022-07-27 23:49:16 +0900

    Create 2804.py

---
+++ 2804.py
@@ -0,0 +1,8 @@
+# 2783번: 삼각 김밥
+
+X, Y = map(int, input().split())
+li = [X / Y]
+for _ in range(int(input())):
+    X, Y = map(int, input().split())
+    li.append(X / Y)
+print("%.2f" % (min(li) * 1000))
