ea603f87ddb7cbc5b1325d997b0e48c6df24306c
myj1217 <myj1217@naver.com>
2022-08-25 23:29:00 +0900

    Create 2991.py

---
+++ 2991.py
@@ -0,0 +1,11 @@
+# 2991번: 사나운 개
+
+A, B, C, D = map(int, input().split())
+li = list(map(int, input().split()))
+for n in li:
+    attacked = 0
+    if 0 < n % (A + B) <= A:
+        attacked += 1
+    if 0 < n % (C + D) <= C:
+        attacked += 1
+    print(attacked)
