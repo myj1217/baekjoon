7fd7aa0f47a806a5b550263bda7469f54f3456a5
myj1217 <myj1217@naver.com>
2022-09-15 14:28:15 +0900

    Create 4299.py

---
+++ 4299.py
@@ -0,0 +1,9 @@
+# 4299번: AFC 윔블던
+
+s, m = map(int, input().split())
+if  s + m < 0 or s - m < 0 or (s + m) % 2:
+    print(-1)
+else:
+    a = (s + m) // 2
+    b = s - a
+    print(max(a, b), min(a, b))
