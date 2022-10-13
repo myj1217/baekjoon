0d8f5e87ba5d9e3f0f79c107d334b3c68837bc07
myj1217 <myj1217@naver.com>
2022-10-13 22:48:44 +0900

    Create 5063.py

---
+++ 5063.py
@@ -0,0 +1,10 @@
+# 5063번: TGN
+
+for _ in range(int(input())):
+    r, e, c = map(int, input().split())
+    if r == e - c:
+        print("does not matter")
+    elif r < e - c:
+        print("advertise")
+    else:
+        print("do not advertise")
