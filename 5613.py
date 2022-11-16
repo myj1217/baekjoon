a919f4900a57902e284061fd0197d25a00f99516
myj1217 <myj1217@naver.com>
2022-11-16 22:54:38 +0900

    Create 5613.py

---
+++ 5613.py
@@ -0,0 +1,13 @@
+# 5613번: 계산기 프로그램
+
+res = int(input())
+while 1:
+    op = input()
+    if op == '=':
+        break
+    n = int(input())
+    if op == '+': res += n;
+    elif op == '-': res -= n;
+    elif op == '*': res *= n;
+    elif op == '/': res //= n;
+print(res)
