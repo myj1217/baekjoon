09eba947ef0990deb1180f9723ef294a86401d23
myj1217 <myj1217@naver.com>
2023-01-04 21:15:44 +0900

    Create 9437.py

---
+++ 9437.py
@@ -0,0 +1,11 @@
+# 9437번: 사라진 페이지 찾기
+
+while 1:
+    t = list(map(int, input().split()))
+    if t == [0]:
+        break
+    for i in range(t[0] // 4):
+        li = [2 * i + 1, 2 * i + 2, t[0] - 2 * i - 1, t[0] - 2 * i]
+        if t[1] in li:
+            li.remove(t[1])
+            print(*li)
