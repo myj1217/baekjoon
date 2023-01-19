171fdaae21e2f5fa29488ec4eaa9024f642a38fb
myj1217 <myj1217@naver.com>
2023-01-19 23:04:28 +0900

    Create 10103.py

---
+++ 10103.py
@@ -0,0 +1,11 @@
+# 10103번: 주사위 게임
+
+c = s = 100
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if a > b:
+        s -= a
+    elif a < b:
+        c -= b
+print(c)
+print(s)
