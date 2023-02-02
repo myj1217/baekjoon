c950da9fc30fba32e11b601b0345a64c0f701c38
myj1217 <myj1217@naver.com>
2023-02-02 22:06:38 +0900

    Create 10409.py

---
+++ 10409.py
@@ -0,0 +1,10 @@
+# 10409번: 서버
+
+n, T = map(int, input().split())
+li = list(map(int, input().split()))
+for i in range(n):
+    if sum(li[:i + 1]) > T:
+        print(i)
+        break
+    elif i == n - 1:
+        print(n)
