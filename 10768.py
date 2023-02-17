2feade5285c30d7f2008ed8a326052e56e475b17
myj1217 <myj1217@naver.com>
2023-02-17 22:46:16 +0900

    Create 10768.py

---
+++ 10768.py
@@ -0,0 +1,15 @@
+# 10768번: 특별한 날
+
+m = int(input())
+d = int(input())
+if m < 2:
+    print("Before")    
+elif m == 2:
+    if d == 18: 
+        print("Special")
+    elif d > 18: 
+        print("After")
+    else: 
+        print("Before")        
+else: 
+    print("After")
