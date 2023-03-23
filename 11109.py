281cd4268c3f7fb28c78100d518d291fa013fae1
myj1217 <myj1217@naver.com>
2023-03-23 20:12:36 +0900

    Create 11109.py

---
+++ 11109.py
@@ -0,0 +1,10 @@
+# 11109번: 괴짜 교수
+
+for _ in range(int(input())):
+    d, n, s, p = map(int, input().split())
+    if d + p * n == n * s:
+        print("does not matter")
+    elif d + p * n > n * s:
+        print("do not parallelize")
+    else:
+        print("parallelize")
